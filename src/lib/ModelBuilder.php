731267d1c0dee500392aee6ba96e53d26a4b43f1
Ibrahim Khalil <msg2ibrahim@hotmail.com>
2023-09-23 14:13:13 +0300

    Revert "v1.1.1 [Checkbox control required prop]"
    This reverts commit 94d5449ce6a3f7027f0401b28b387915097563c9.

--- src/lib/ModelBuilder.php
+++ src/lib/ModelBuilder.php
@@ -184,7 +184,7 @@ class ModelBuilder
 
             if (! $customFld->IsCustom()) {
 
-                $props[] = $this->IsRequired($db_fld, $control);
+                $props[] = $this->IsRequired($db_fld);
 
                 $props[] = $this->DefaultValue($db_fld);
 
@@ -257,9 +257,9 @@ class ModelBuilder
     }
 
 
-    protected function IsRequired($db_fld, $control)
+    protected function IsRequired($db_fld)
     {
-        return $control === 'Checkbox' ? (($db_fld['Null'] == 'NO') ? '' : '->setRequired(false)') : (($db_fld['Null'] == 'NO') ? '->setRequired()' : '');
+        return ($db_fld['Null'] == 'NO') ? '->setRequired()' : '';
     }
 
 
